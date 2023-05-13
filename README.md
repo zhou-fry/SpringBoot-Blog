5c626d1d85ee7368a56004239604193e1d58396f
zhoubaofeng <zhoubaofeng@joyintech.com>
2023-05-13 17:49:41 +0800

    项目内容调整，修改框架结构

--- README.md
+++
@@ -1,2 +0,0 @@
-# SpringBoot-Blog
-SpringBoot开发一版博客，边学习边上传
---
+++ src/test/java/com/cn/zy/zbf/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cn.zy.zbf.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
