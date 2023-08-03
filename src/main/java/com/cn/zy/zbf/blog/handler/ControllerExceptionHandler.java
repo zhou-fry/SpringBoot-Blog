1aae3b5cfdc07837415a55fa837a050c18fbb81e
fry-zhou <1156448401@qq.com>
2023-08-03 23:05:50 +0800

    新机器做测试

--- src/main/java/com/cn/zy/zbf/blog/handler/ControllerExceptionHandler.java
+++ src/main/java/com/cn/zy/zbf/blog/handler/ControllerExceptionHandler.java
@@ -35,6 +35,7 @@ public class ControllerExceptionHandler {
         mv.addObject("url",request.getRequestURL());
         mv.addObject("exception", e);
         mv.setViewName("error/error");
+        System.out.println("新机器做测试");
         return mv;
     }
 }
